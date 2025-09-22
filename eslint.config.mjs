3d25e518d2c06334c11e34af2159e681e13e034a
leon-dream1 <nahidulislam019045@gmail.com>
2025-09-22 22:09:27 +0600

    server initial setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+
+  {
+    ignores: ['node_modules', 'dist'],
+    rules: {
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+    },
+  },
+]);
